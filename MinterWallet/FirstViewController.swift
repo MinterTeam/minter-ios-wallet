71f1449c521dfaf55edd10fbd71a591ac2882e3f
Alexey Sidorov <ody344@gmail.com>
2018-04-02 13:14:12 +0300

    Initial Commit

---
+++ MinterWallet/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  MinterWallet
+//
+//  Created by Alexey Sidorov on 02/04/2018.
+//  Copyright © 2018 Minter. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
---
+++ MinterWallet/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  MinterWallet
+//
+//  Created by Alexey Sidorov on 02/04/2018.
+//  Copyright © 2018 Minter. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
